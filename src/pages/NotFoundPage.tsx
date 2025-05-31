f7632365feb7877b20ce56b84ebeedea81e3e938
Oleg Olefirenko <olegolefirenko@MacBook-Pro.local>
2025-05-31 08:45:59 +0200

    Просто комментарий

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const NotFoundPage: React.FC = () => {
+    // TODO: add a not found page
   return (
     <div className="p-8 text-center">
       <h2 className="text-2xl font-bold mb-4 text-red-600">404 — Страница не найдена</h2>
